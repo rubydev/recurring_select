d7a8350103b8c3d67081472654aa24254085ef6a
Marian Mrózek <mrozek.marian@gmail.com>
2018-07-19 11:17:35 +0200

    Rails 5.2 compatibility

--- app/helpers/recurring_select_helper.rb
+++ app/helpers/recurring_select_helper.rb
@@ -86,7 +86,11 @@ module RecurringSelectHelper
     end
 
     def render
-      option_tags = add_options(recurring_options_for_select(value(object), @default_schedules, @options), @options, value(object))
+      if Rails::VERSION::STRING >= '5.2'
+        option_tags = add_options(recurring_options_for_select(value, @default_schedules, @options), @options, value)
+      else
+        option_tags = add_options(recurring_options_for_select(value(object), @default_schedules, @options), @options, value(object))
+      end
       select_content_tag(option_tags, @options, @html_options)
     end
 
